15b17e36a4c56f7d9434de56428b455d63928263
pie <rm8l@outlook.com>
2024-08-09 14:44:12 -0300

    fix: remove teste page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Teste from '../pages/teste'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-  <>
-    <div>
-      hello world
-    </div>
-  </>
+    <>
+      <div>
+        hello world
+      </div>
+    </>
   )
 }
