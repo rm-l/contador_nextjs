567628768c043ed04b8f44d1095c4c002ea246b6
pie <rm8l@outlook.com>
2024-08-09 16:15:48 -0300

    feat: home page && css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
-import Image from 'next/image'
+'use client'
+import { useRouter } from 'next/navigation';
 
-export default function Home() {
+const MeuComponente = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/contador');
+  };
+
   return (
-    <>
-      <div>
-        hello world
+    <div className='min-h-screen min-w-full bg-gray-900'>
+
+      <div className='flex flex-col items-center bg-gray-800 text-gray-200 py-10'>
+        <h1 className='text-4xl font-bold mb-4'>
+          Olá, esta é uma aplicação simples.
+        </h1>
+        <h2 className='text-2xl'>
+          Criada para contar palavras, espaços, consoantes e vogais.
+        </h2>
       </div>
-    </>
-  )
-}
+
+      <div className='flex justify-center p-10'>
+        <button
+          type='button'
+          className='bg-gray-700 text-gray-200 font-bold py-2 px-4 rounded hover:bg-gray-600 transition duration-300'
+          onClick={handleClick}>
+          Acessar o Contador
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MeuComponente;
--- src/pages/contador.tsx
+++ src/pages/contador.tsx
@@ -1,8 +1,7 @@
-import { useState, ChangeEvent } from 'react'
-import '../app/globals.css'
+import { useState, ChangeEvent } from 'react';
+import '../app/globals.css';
 
 export default function Teste() {
-
     const [text, setText] = useState('');
 
     const wordCount = text.trim() === '' ? 0 : text.trim().split(/\s+/).length;
@@ -15,40 +14,38 @@ export default function Teste() {
     };
 
     return (
-        <div className='flex flex-col min-h-screen min-w-screen'>
+        <div className='flex flex-col min-h-screen min-w-full bg-gray-900 text-gray-200'>
 
-            <div className='flex flex-col min-h-screen min-w-screen align-middle justify-center'>
-                <div className='flex justify-center p-5 flex-col'>
-                    <div className='flex justify-center p-5'>
+            <div className='flex flex-col items-center justify-center min-h-screen min-w-full'>
+                <div className='flex flex-col w-[60%]'>
+                    <div className='mb-5'>
                         <textarea
                             value={text}
                             onChange={handleChange}
-                            className='text-gray-800 w-[50%] h-[300px] mx-auto p-2'
+                            className='w-full h-[300px] p-4 bg-gray-800 text-gray-200 border border-gray-600 rounded resize-none'
                             placeholder='Digite aqui...'
                         ></textarea>
                     </div>
-                    <div className='flex min-w-full min-h-full justify-center p-5'>
-                        <div className='flex flex-col min-w-[50%] justify-center'>
-                            <div className='bg-slate-200 flex flex-row'>
-                                <div className='bg-slate-400 w-[50%] p-1'>
-                                    Palavras: {wordCount}
-                                </div>
-                                <div className='text-gray-800 flex w-[50%] p-1'>
-                                    Espaços: {spaceCount}
-                                </div>
+                    <div className='bg-gray-800 p-4 rounded'>
+                        <div className='flex flex-row justify-between border-b border-gray-700 pb-2 mb-2'>
+                            <div className='w-[50%]'>
+                                <span className='font-bold'>Palavras: </span>{wordCount}
                             </div>
-                            <div className='bg-slate-200 flex flex-row min-w-full'>
-                                <div className='text-gray-800 flex w-[50%] p-1'>
-                                    Consoantes: {consonantCount}
-                                </div>
-                                <div className='bg-slate-400 w-[50%] p-1'>
-                                    Vogais: {vowelCount}
-                                </div>
+                            <div className='w-[50%] text-right'>
+                                <span className='font-bold'>Espaços: </span>{spaceCount}
+                            </div>
+                        </div>
+                        <div className='flex flex-row justify-between'>
+                            <div className='w-[50%]'>
+                                <span className='font-bold'>Consoantes: </span>{consonantCount}
+                            </div>
+                            <div className='w-[50%] text-right'>
+                                <span className='font-bold'>Vogais: </span>{vowelCount}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
